50a1c9cc69449dadb76f0450bd227e7f4ed35e13
johnn3volta <johnn3volta@gmail.com>
2018-12-07 17:25:22 +0300

    try creat recursive

--- index.php
+++ index.php
@@ -1,17 +1,67 @@
 <?php
-$dir ='wada';
-$iter = new RecursiveIteratorIterator(
-  new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS),
-  RecursiveIteratorIterator::SELF_FIRST
-  // при блоке прав чтения не отвалится
+$dird = 'ddd';
 
-);
+$json =[];
 
-$paths = array($dir);
-foreach ($iter as $path => $dir) {
-    if ($dir->isDir()) {
-        $paths[] = $path;
+function PrintDirectoryTree($startDir, $parent = 0, &$arr = [])
+{
+    if($parent == 0){
+        $json[]['name'] = $startDir;
     }
+    $dirs = array_diff(scandir($startDir), ['..', '.']);
+    echo count($dirs) . '<br>';
+    $i = 0;
+    foreach ($dirs as $dir) {
+        $pathDir = $startDir . DIRECTORY_SEPARATOR . $dir;
+        if(is_dir($pathDir)){
+            $arr[$parent]['childrens'][$i]['name'] = $dir;
+            $arr[$parent]['childrens'][$i]['path'] = $pathDir;
+
+            PrintDirectoryTree($pathDir, 1, $json);
+        }
+        $i++;
+    }
+
+    $json = $arr;
+
+    return $json;
+    ////    echo "</ul>";
+    ////    echo "</li>";
+    //    if($parent == 0){
+    //        echo "</ul>";
+    //    }
 }
-print_r($paths);
+
+echo '<pre>';
+print_r(PrintDirectoryTree($dird));
+
+
+//function PrintDirectoryTree($startDir, $parent = 0,$name = '')
+//{
+//    if($parent == 0){
+//        echo "<ul id='jsTree'>";
+//    }
+//    $pathName = !$name ? $startDir : $name;
+//    echo "<li data-dirPath='$startDir'>$pathName";
+//    echo "<ul>";
+//    $dirs = array_diff(scandir($startDir),array('..','.'));
+//    foreach ($dirs as $dir) {
+//        $pathDir = $startDir . DIRECTORY_SEPARATOR . $dir;
+//        if(is_dir($pathDir)){
+//            PrintDirectoryTree($pathDir, 1,$dir);
+//        }
+//    }
+//    echo "</ul>";
+//    echo "</li>";
+//    if($parent == 0){
+//        echo "</ul>";
+//    }
+//}
+//
+//PrintDirectoryTree($dir);
+
+
+
+
+
 
